4f8cc25ee1ecbcb109e6c71a0f47f2d1b899a4ce
Matheus Estrela <mr.estrela@yahoo.com.br>
2021-12-04 16:18:30 -0300

    C:/Users/kdes/OneDrive/Área de Trabalho/Pj/create.py

---
+++ create.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import datetime
+
+from mysql.connector.dbapi import DATETIME
+
+connection = mysql.connector.connect( 
+    host="locahost",
+    user="usuariobancodados",
+    password="senhausuario",
+    database="bancodados"
+)
+
+cursor = connection.cursor()
+
+sql =  "INSERT INTO users (nome, email, created)  VALUES (%s, %s, %s)"
+data = (
+  "primeiro usuario",
+  "primeirousuario@teste.com.br",
+  DATETIME.datatime.today()
+)
+
+cursor.exexute(sql, data)
+connection.commit()
+
+userid = cursor.lastrowid
+
+cursor.close()
+connection.close()
+
+print("foi cadastrado o novo usuario de ID:", userid)
